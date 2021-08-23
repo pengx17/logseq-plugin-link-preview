64b8e3dee1f59e33dd204a6705b1a1fcaa29f6d0
Peng Xiao <pengxiao@outlook.com>
2021-08-23 23:35:10 +0800

    feat: add convert to link card command

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,27 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { isInlineMode } from "./utils";
 
+const urlRegex =
+  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/;
+
 function main(inline: boolean) {
+  if (!inline) {
+    logseq.Editor.registerSlashCommand("Convert to Link Card 🪧", async () => {
+      const maybeUrl = (await logseq.Editor.getEditingBlockContent()).trim();
+      const id = await logseq.Editor.getCurrentBlock();
+
+      if (urlRegex.test(maybeUrl) && id) {
+        const newContent = ` <iframe data-url='${maybeUrl}' src='${logseq.baseInfo.entry}'></iframe> `;
+        logseq.Editor.updateBlock(id.uuid, newContent);
+      } else {
+        logseq.App.showMsg(
+          "The block content does not seem to be a valid URL",
+          "warning"
+        );
+      }
+    });
+  }
+
   ReactDOM.render(
     <React.StrictMode>
       <App inline={inline} />
